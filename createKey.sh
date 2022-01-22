bacc9e87f0798bbd8bcd933715c2e334b73557ac
cccsss01 <56396984+cccsss01@users.noreply.github.com>
2022-01-22 12:28:16 -0600

    Update createKey.sh

--- createKey.sh
+++ createKey.sh
@@ -3,3 +3,12 @@ mv user.key ./users
 
 
 openssl req -new -key user.key -out user.csr -sub "/CN=user/O=ops/O=all"
+
+#ca cert and key located in /etc/kuberentes/pki for kubeadm
+openssl x509 -req -CA ca.pem -CAkey ca-key.pem -CAcreateserial -days 730 -in user.csr -out user.crt
+
+#set credentials in cluster
+kubectl config set-credentials user-prod --client-sertificate=/home/vagrant/.kube/users/user.crt --client-key=/home/vagrant/.kube/users/user.pem
+
+#set context
+k config set-context user-prod --cluster=prod--user=user --namespace=optional
